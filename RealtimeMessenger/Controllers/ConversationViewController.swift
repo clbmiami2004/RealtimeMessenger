2e347d15b28e64ad85f667c3b1806689e92264fb
Christian Lorenzo <clbmiami2004@gmail.com>
2021-08-31 15:28:23 -0500

    Completed register view controller, and log in view controller. Created log in button and register button. Modified the corner radius of button objects. Added a message image to the files.

--- RealtimeMessenger/Controllers/ConversationViewController.swift
+++ RealtimeMessenger/Controllers/ConversationViewController.swift
@@ -11,8 +11,23 @@ class ConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "Logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+    
 
 
 }
---
+++ RealtimeMessenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  RealtimeMessenger
+//Users/christianlorenzo-denso/Library/Mobile Documents/com~apple~CloudDocs/Development iCloud/Personal Projects/RealtimeMessenger/RealtimeMessenger/Controllers/Login/LoginViewController.swift//
+//  Created by Christian Lorenzo on 8/19/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+
+}
